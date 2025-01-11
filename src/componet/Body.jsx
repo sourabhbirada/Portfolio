f086941d6d7abe987caabeb261695092089d7ecb
Sourabh <72642622+sourabhbirada@users.noreply.github.com>
2025-01-11 20:55:58 +0530

    Update Body.jsx

--- src/componet/Body.jsx
+++ src/componet/Body.jsx
@@ -32,11 +32,11 @@ const Body = () => {
   };
   return (
     <div  className="ml-10 mt-6  text-[20px]">
-      {/* {showWarning && (
+       {showWarning && (
                 <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
                     <div className="bg-white p-8 rounded-lg w-96 text-center relative">
                         <h2 className="text-lg font-semibold mb-4">Warning</h2>
-                        <p className='font-merriweather'>I am currently working on this site and learning frontend development.</p>
+                        <p className='font-merriweather'>I am currently working on this site.</p>
                         <span 
                             className="absolute top-2 right-2 text-2xl cursor-pointer"
                             onClick={handleCloseWarning}
@@ -45,7 +45,7 @@ const Body = () => {
                         </span>
                     </div>
                 </div>
-            )} */}
+            )} 
       <h3 className="mb-4 font-dmSerif">Wanna see some projects?</h3>
       <div className="flex flex-col font-dmSerif text-[18px] mb-3 gap-2">
       <label>
